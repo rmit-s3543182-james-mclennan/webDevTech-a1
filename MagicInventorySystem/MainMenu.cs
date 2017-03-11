6cfd5140b1ebe6bc368819bd15899db010372a80
rmit-s3543182-james-mclennan <s3543182@student.rmit.edu.au>
2017-03-12 10:35:29 +1100

    Implemented first push with classes
    Pushed Main (Driver) and MainMenu classes to dev branch.

---
+++ MagicInventorySystem/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MagicInventorySystem
+{
+    internal class MainMenu
+    {
+        public void launchMenu()
+        {
+            Console.WriteLine("Welcome to the Magic Inventory System!");
+            Console.ReadKey();
+        }
+    }
+}
