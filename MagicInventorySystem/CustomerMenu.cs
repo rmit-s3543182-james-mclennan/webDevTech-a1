b410e8707755ae2ce923eab0a99e2d253169c050
cre8iveallen <pyeinkiller@gmail.com>
2017-03-29 11:03:17 +1100

    Add files via upload

--- MagicInventorySystem/CustomerMenu.cs
+++ MagicInventorySystem/CustomerMenu.cs
@@ -8,6 +8,8 @@ namespace MagicInventorySystem
 {
     class CustomerMenu
     {
+        JsonProcessor reader = new JsonProcessor();
+        Owner displayProducts = new Owner();
         public MagicMenuCollection loadCustomerMenu(MagicMenuCollection collection)
         {
             collection.Menus.Add(new MagicMenuList()
@@ -21,7 +23,12 @@ namespace MagicInventorySystem
                             CannotExecute = false,
                             Execute = () =>
                             {
-                                Console.WriteLine("Need to implement function!");
+                                Console.Clear();
+                                displayProducts.displayAllStock();
+                                CustomerOrder();
+                                Console.ReadKey();
+                                Console.Clear();
+                                collection.ShowMenu(4);
                             }
                         },
                         new MagicMenuItem()
@@ -54,5 +61,61 @@ namespace MagicInventorySystem
 
             return collection;
         }
+
+        public void CustomerOrder()
+        {
+            int isComplete = 0;
+            while(isComplete == 0)
+            {
+                Console.WriteLine("[Legend: 'P' Next Page | 'R' Return to Menu | 'C' Complete Transaction]");
+                Console.Write("Enter Item Number to purchase or Function: ");
+                string choice = Console.ReadLine();
+                if (choice == "P" || choice == "p")
+                {
+                    // Get to next product page
+                    Console.Clear();
+                    displayProducts.displayAllStock();
+                    Console.WriteLine("got to next product page");
+                    moveProductPage();
+                    isComplete = 0;   
+                }
+                else if (choice == "R" || choice == "r")
+                {
+                    // Get back to previous product page
+                    Console.Clear();
+                    displayProducts.displayAllStock();
+                    Console.WriteLine("Got back to previous page");
+                    moveProductPage();
+                    
+                    isComplete = 0;
+                }
+                else if (choice == "C" || choice == "c")
+                {
+                    // Transaction done
+                    Console.WriteLine("need to implement function");
+                    isComplete = 1;
+                }
+                else
+                {
+                    // Invalid input. try again
+                    Console.Clear();
+                    displayProducts.displayAllStock();
+                    Console.WriteLine("Invalid input - try again");
+                    isComplete = 0;
+
+                }
+            }
+
+        }
+
+        public void moveProductPage()
+        {
+            /* need to implement function to paginate json file objects
+             * to indicate 5 products in a page
+             */
+              
+             
+        }
+
     }
 }
