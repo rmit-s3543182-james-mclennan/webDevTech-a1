e978971b0452ca931e81573801abf3f0ac2c384d
cre8iveallen <pyeinkiller@gmail.com>
2017-03-31 18:16:07 +1100

    page implementation done

--- MagicInventorySystem/CustomerMenu.cs
+++ MagicInventorySystem/CustomerMenu.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace MagicInventorySystem
 {
-    class CustomerMenu : PageLoad
+    class CustomerMenu
     {
         JsonProcessor reader = new JsonProcessor();
         PageLoad pageLoad = new PageLoad();
@@ -25,13 +25,6 @@ namespace MagicInventorySystem
                             Option = "Display Products\n",
                             CannotExecute = true,
                             SubMenuId = 6
-
-                                //Console.Clear();
-                                //CustomerOrder();
-                                //Console.ReadKey();
-                                //Console.Clear();
-                                //collection.ShowMenu(4);
-                            
                         },
                         new MagicMenuItem()
                         {
@@ -67,79 +60,7 @@ namespace MagicInventorySystem
             
         
 
-        public void CustomerOrder()
-        {
-            List<Products> allStock = JsonConvert.DeserializeObject<List<Products>>(File.ReadAllText("owners_inventory.json"));
-            totalPage = 4;
-            currentPage = 1;
-            isCompleted = 0;
-
-            Console.Clear();
-            Console.WriteLine("[!] Loading products from owners_inventory.json");
-            displayTitle();
-            currentPage = pageOne();
-            isCompleted = 0;
-            while (isCompleted == 0)
-            {
-                Console.WriteLine("Page " + currentPage + "/" + totalPage);
-                Console.WriteLine("[Legend: 'P' Next Page | 'R' Return to Menu | 'C' Complete Transaction]");
-                Console.Write("Enter Item Number to purchase or Function(ID - Quantity) : ");
-                choice = Console.ReadLine();
-                if (choice == "P" || choice == "p")
-                {
-                    isCompleted = displayPageTwo();
-                }
-                else if (choice == "R" || choice == "r")
-                {
-                    isCompleted = displayPageFour();
-                }
-                else if (choice == "C" || choice == "c")
-                {
-                    isCompleted = transactionComplete();
-                }
-                //else
-                //{
-                //    isCompleted = invalidInput();
-                //    Console.WriteLine("Invalid Input. Try again.");
-                //}
-
-                /* compare product request with current stocklevel
-                 * and then if stocklevel is > 0
-                 * update current stock(-1)
-                 * afterwards ask again to buy more stuff
-                 * and book a workshop(if so, 10% discount of price)
-                 */
-                foreach (Products getItem in allStock)
-                {
-                    // compare input with getItem ID and stockLevel > 0
-                    if (choice == Convert.ToString(getItem.ID)
-                    && getItem.stockLevel > 0)
-                    {
-                        Console.Write("Choose the quantity of the product(current stock : "
-                            + getItem.stockLevel + ") : ");
-                        choice = Console.ReadLine();
-                        //switch(choice)
-                        //{
-                        //    case "1":
-                        //        break;
-                        //    case "2":
-                        //        break;
-                        //    case "3":
-                        //        break;
-                        //        case
-
-                        //}
-                        
-                    }
-                    // out of stock
-                    else
-                    {
-                        Console.WriteLine("The product is currently out of stock!");
-                    }
-                }
-            }
-
-        }
+        
     }
     
 }
--- MagicInventorySystem/franchiseCustomer.cs
+++ MagicInventorySystem/franchiseCustomer.cs
@@ -9,7 +9,7 @@ using System.IO;
 namespace MagicInventorySystem
 {
 
-    class franchiseCustomer
+    class franchiseCustomer : PageLoad
     {
         FranchiseOwner access = new FranchiseOwner();
         private String storeID;
@@ -26,9 +26,11 @@ namespace MagicInventorySystem
                     CannotExecute = false,
                     Execute = () =>
                     {
-                        Console.Clear();
-                        storeID = access.identifyStore("Melbourne_CBD");
-                        collection.ShowMenu(3);
+                                Console.Clear();
+                                CustomerOrder();
+                                Console.ReadKey();
+                                Console.Clear();
+                                collection.ShowMenu(6);
 
                     }
                 },
@@ -39,8 +41,10 @@ namespace MagicInventorySystem
                             Execute = () =>
                             {
                                 Console.Clear();
-                                storeID = access.identifyStore("Melbourne_North");
-                                collection.ShowMenu(3);
+                                CustomerOrder();
+                                Console.ReadKey();
+                                Console.Clear();
+                                collection.ShowMenu(6);
                             }
                         },
                         new MagicMenuItem()
@@ -50,8 +54,10 @@ namespace MagicInventorySystem
                             Execute = () =>
                             {
                                 Console.Clear();
-                                storeID = access.identifyStore("Melbourne_South");
-                                collection.ShowMenu(3);
+                                CustomerOrder();
+                                Console.ReadKey();
+                                Console.Clear();
+                                collection.ShowMenu(6);
                             }
                         },
                         new MagicMenuItem()
@@ -61,8 +67,10 @@ namespace MagicInventorySystem
                             Execute = () =>
                             {
                                 Console.Clear();
-                                storeID = access.identifyStore("Melbourne_East");
-                                collection.ShowMenu(3);
+                                CustomerOrder();
+                                Console.ReadKey();
+                                Console.Clear();
+                                collection.ShowMenu(6);
                             }
                         },
                         new MagicMenuItem()
@@ -72,8 +80,10 @@ namespace MagicInventorySystem
                             Execute = () =>
                             {
                                 Console.Clear();
-                                storeID = access.identifyStore("Melbourne_West");
-                                collection.ShowMenu(3);
+                                CustomerOrder();
+                                Console.ReadKey();
+                                Console.Clear();
+                                collection.ShowMenu(6);
                             }
                         },
                         new MagicMenuItem()
@@ -96,6 +106,80 @@ namespace MagicInventorySystem
             });
 
             return collection;
+        }
+
+        public void CustomerOrder()
+        {
+            List<Products> allStock = JsonConvert.DeserializeObject<List<Products>>(File.ReadAllText("owners_inventory.json"));
+            totalPage = 4;
+            currentPage = 1;
+            isCompleted = 0;
+
+            Console.Clear();
+            Console.WriteLine("[!] Loading products from owners_inventory.json");
+            displayTitle();
+            currentPage = pageOne();
+            isCompleted = 0;
+            while (isCompleted == 0)
+            {
+                Console.WriteLine("Page " + currentPage + "/" + totalPage);
+                Console.WriteLine("[Legend: 'P' Next Page | 'R' Return to Menu | 'C' Complete Transaction]");
+                Console.Write("Enter Item Number to purchase or Function(ID - Quantity) : ");
+                choice = Console.ReadLine();
+                if (choice == "P" || choice == "p")
+                {
+                    isCompleted = displayPageTwo();
+                }
+                else if (choice == "R" || choice == "r")
+                {
+                    isCompleted = displayPageFour();
+                }
+                else if (choice == "C" || choice == "c")
+                {
+                    isCompleted = transactionComplete();
+                }
+                //else
+                //{
+                //    isCompleted = invalidInput();
+                //    Console.WriteLine("Invalid Input. Try again.");
+                //}
+
+                /* compare product request with current stocklevel
+                 * and then if stocklevel is > 0
+                 * update current stock(-1)
+                 * afterwards ask again to buy more stuff
+                 * and book a workshop(if so, 10% discount of price)
+                 */
+                foreach (Products getItem in allStock)
+                {
+                    // compare input with getItem ID and stockLevel > 0
+                    if (choice == Convert.ToString(getItem.ID)
+                    && getItem.stockLevel > 0)
+                    {
+                        Console.Write("Choose the quantity of the product(current stock : "
+                            + getItem.stockLevel + ") : ");
+                        choice = Console.ReadLine();
+                        //switch(choice)
+                        //{
+                        //    case "1":
+                        //        break;
+                        //    case "2":
+                        //        break;
+                        //    case "3":
+                        //        break;
+                        //        case
+
+                        //}
+
+                    }
+                    // out of stock
+                    else
+                    {
+                        Console.WriteLine("The product is currently out of stock!");
+                    }
+                }
+            }
+
         }
     }
         
