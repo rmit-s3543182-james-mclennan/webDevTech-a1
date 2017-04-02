d179882236a3c64cb18f68772d20a2025fd253dc
cre8iveallen <pyeinkiller@gmail.com>
2017-04-02 19:39:25 +1000

    started working on customerWorkshop class

--- MagicInventorySystem/PageLoad.cs
+++ MagicInventorySystem/PageLoad.cs
@@ -24,6 +24,7 @@ namespace MagicInventorySystem
         public string storeFileName { get; set; }
         public string choice { get; set; }
 
+        public bool workshopBookingCheck { get; set; }
         public List<Products> allStock { get; set; }
         public List<Products> soldItems { get; set; }
         JsonProcessor reader = new JsonProcessor();
@@ -40,124 +41,6 @@ namespace MagicInventorySystem
             lastItem = 5;
         }
 
-        private void purchaseProgress(int amount, int currentItemIndex)
-        {
-            allStock[currentItemIndex].stockLevel -= amount;
-            File.WriteAllText(storeFileName, JsonConvert.SerializeObject(allStock, Formatting.Indented));
-            purchaseCount++;
-        }
-
-        public int displayPurchaseSummary()
-        {
-            Console.WriteLine("You have purchased : \n");
-            try
-            {
-                for(int i = 0; i < purchaseItemIndex; i++)
-                {
-                    Console.WriteLine(soldItems[i].name + " " + soldItems[i].stockLevel + "ea\n");
-                }
-            }
-            catch(ArgumentOutOfRangeException e)
-            {
-
-            }
-            Console.Write("Would you like to book a workshop?(Y / N) : ");
-            choice = Console.ReadLine();
-            if (choice == "Y" || choice == "y")
-            {
-                Console.WriteLine("The total price of your purchase has got 10% discount!");
-                transactionComplete();
-            }
-            else if (choice == "N" || choice == "n")
-            {
-                transactionComplete();
-            }
-            else
-            {
-                invalidInput();
-            }
-
-
-            return isCompleted;
-        }
-
-        public int purchaseItems(int choiceIndex)
-        {
-            currentItemIndex = choiceIndex;
-            Console.WriteLine("You have chosen " + allStock[currentItemIndex].name + ".");
-            Console.Write("Enter the amount of the product : ");
-            choice =  Console.ReadLine();
-            // if the item exists(true), ask how many to get
-            if (int.TryParse(choice, out choiceIndex)
-            && choiceIndex > 0
-            && choiceIndex <= allStock[currentItemIndex].stockLevel)
-            {
-                /* Deduct the amount of the product from appropriate store json file
-                 * and purchasedItem++
-                 */
-                purchaseProgress(choiceIndex, currentItemIndex);    // works
-                Console.WriteLine("You have purchased " + choiceIndex + "ea of " + allStock[currentItemIndex].name);
-                soldItems[purchaseItemIndex].name = allStock[currentItemIndex].name;
-                try
-                {
-                    soldItems[purchaseItemIndex].stockLevel = choiceIndex;
-                    purchaseItemIndex++;
-                }catch(ArgumentOutOfRangeException e)
-                {
-
-                }
-                
-
-
-                // Ask again whether the customer wants to buy more products, if so purchaseItem++
-                Console.Write("Do you want to buy more products?(Y / N) : ");
-                choice = Console.ReadLine();
-                if(choice == "Y" || choice == "y")
-                {
-                    Console.Clear();
-                    currentPage();
-                }
-                else if(choice == "N" || choice == "n")
-                {
-                    Console.Write("Do you want to pay for credit card?(Y / N) : ");
-                    choice = Console.ReadLine();
-                    if (choice == "Y" || choice == "y")
-                    {
-                        Console.WriteLine("Credit card payment is being processed!");
-                    }
-                    else if(choice == "N" || choice == "n")
-                    {
-                        Console.WriteLine("Cash payment is being processed!");
-                    }
-                    displayPurchaseSummary();
-                }
-                else
-                {
-                    invalidInput();
-                }
-
-                /* if the user enters c for transaction complete
-                 * , display summary of the purchase
-                 * also ask him if he wants to book a workshop
-                 */
-
-                /* if so, keep the purchasedItem value
-                 * and give him 10% discount by displaying in the summary
-                 */
-            }
-            // else if the item is out of stock, print it is out of stock
-            else if(allStock[currentItemIndex].stockLevel <= 0)
-            {
-                outOfStock();
-            }
-            // else input is invalid
-            else
-            {
-                invalidInput();
-            }
-            return isCompleted;
-        }
-
         public int firstPage()
         {
             allStock = JsonConvert.DeserializeObject<List<Products>>(File.ReadAllText(storeFileName));
@@ -168,7 +51,7 @@ namespace MagicInventorySystem
                 totalPage += 1;
             }
 
-            if(allStock.Count < 5)
+            if (allStock.Count < 5)
             {
                 lastItem = allStock.Count;
             }
@@ -246,8 +129,8 @@ namespace MagicInventorySystem
                 pageIndex = totalPage;
             }
             displayTitle();
-            
-            if(lastItem < 5)
+
+            if (lastItem < 5)
             {
                 firstItem = 0;
             }
@@ -283,7 +166,7 @@ namespace MagicInventorySystem
             Console.WriteLine("Page " + pageIndex + "/" + totalPage);
             Console.WriteLine("[Legend: 'P' Next Page | 'R' Return to Menu | 'C' Complete Transaction]");
             Console.Write("Enter Product ID to purchase : ");
-            
+
             choice = Console.ReadLine();
             return isCompleted;
         }
@@ -374,12 +257,12 @@ namespace MagicInventorySystem
         {
             Console.Clear();
             Console.WriteLine("Invalid input. Try again.");
-            
-            if(firstItem < 1)
+
+            if (firstItem < 1)
             {
                 firstPage();
             }
-            else if(lastItem >= allStock.Count)
+            else if (lastItem >= allStock.Count)
             {
                 lastPage();
             }
@@ -416,6 +299,116 @@ namespace MagicInventorySystem
                     Console.Write("=\n");
                 }
             }
+        }
+
+        public int purchaseItems(int choiceIndex)
+        {
+            currentItemIndex = choiceIndex;
+            Console.WriteLine("You have chosen " + allStock[currentItemIndex].name + ".");
+            Console.Write("Enter the amount of the product : ");
+            choice = Console.ReadLine();
+            // if the item exists(true), ask how many to get
+            if (int.TryParse(choice, out choiceIndex)
+            && choiceIndex > 0
+            && choiceIndex <= allStock[currentItemIndex].stockLevel)
+            {
+                /* Deduct the amount of the product from appropriate store json file
+                 * and purchasedItem++
+                 */
+                purchaseProgress(choiceIndex, currentItemIndex);    // works
+                Console.WriteLine("You have purchased " + choiceIndex + "ea of " + allStock[currentItemIndex].name);
+                soldItems[purchaseItemIndex].name = allStock[currentItemIndex].name;
+                try
+                {
+                    soldItems[purchaseItemIndex].stockLevel = choiceIndex;
+                    purchaseItemIndex++;
+                }
+                catch (ArgumentOutOfRangeException e)
+                {
+
+                }
+                Console.Write("Do you want to buy more products?(Y / N) : ");
+                choice = Console.ReadLine();
+                if (choice == "Y" || choice == "y")
+                {
+                    Console.Clear();
+                    currentPage();
+                }
+                else if (choice == "N" || choice == "n")
+                {
+                    Console.Write("Do you want to pay for credit card?(Y / N) : ");
+                    choice = Console.ReadLine();
+                    if (choice == "Y" || choice == "y")
+                    {
+                        Console.WriteLine("Credit card payment is being processed!");
+                    }
+                    else if (choice == "N" || choice == "n")
+                    {
+                        Console.WriteLine("Cash payment is being processed!");
+                    }
+                    displayPurchaseSummary();
+                }
+                else
+                {
+                    invalidInput();
+                }
+            }
+            else if (allStock[currentItemIndex].stockLevel <= 0)
+            {
+                outOfStock();
+            }
+            else
+            {
+                invalidInput();
+            }
+            return isCompleted;
+        }
+
+        private void purchaseProgress(int amount, int currentItemIndex)
+        {
+            allStock[currentItemIndex].stockLevel -= amount;
+            File.WriteAllText(storeFileName, JsonConvert.SerializeObject(allStock, Formatting.Indented));
+            purchaseCount++;
+        }
+
+        public int displayPurchaseSummary()
+        {
+            Console.Write("Would you like to book a workshop?(Y / N) : ");
+            choice = Console.ReadLine();
+            if ((choice == "Y" || choice == "y") && purchaseItemIndex > 0)
+            {
+                Console.WriteLine("The total price of your purchase has got 10% discount!");
+                workshopBookingCheck = true;
+            }
+            else if (choice == "N" || choice == "n")
+            {
+                Console.WriteLine("Workshop is not booked!");
+                workshopBookingCheck = false;
+            }
+            else
+            {
+                invalidInput();
+            }
+            Console.WriteLine("========== Purchase Summary ==========");
+            Console.WriteLine("You have purchased : \n");
+            try
+            {
+                for(int i = 0; i < purchaseItemIndex; i++)
+                {
+                    Console.WriteLine(soldItems[i].name + " " + soldItems[i].stockLevel + "ea");
+                }
+                if(workshopBookingCheck == true)
+                {
+                    Console.WriteLine("Workshop booked, 10% discount of total price.");
+                }
+                Console.WriteLine("======================================\n");
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+
+            }
+            transactionComplete();
+            return isCompleted;
         }
     }
 }
